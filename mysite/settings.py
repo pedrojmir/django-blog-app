18b9618c97f8ee34fc1dc665bbae01d5f9e3f4f2
Pedro Mir Reynoso <pedrojmir@gmail.com>
2023-12-19 16:01:19 -0500

    updated dotenv settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', os.urandom(24).hex())
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-1iv+__w%$5-l1zm0#7!isj%))))gnjdd30u&v14cb=-f1&t*+b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
