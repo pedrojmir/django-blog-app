aa889c1fc422b7e6dcfff93e27e0b86d0e718e7b
Pedro Mir Reynoso <pedrojmir@gmail.com>
2023-12-19 15:16:30 -0500

    updated secret key settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', os.urandom(24).hex())
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', False) == 'True'
